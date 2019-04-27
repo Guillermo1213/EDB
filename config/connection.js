2c1eb98a610299a9339fb4d67f4e247552d24cce
Guillermo Garcia Jr <guillermo@Guillermos-MacBook-Pro.local>
2019-04-27 15:19:28 -0700

    Added JAWSDB and updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,25 @@ var mysql = require('mysql');
 
 var connection;
 
-var connection = mysql.createConnection({
+connection = mysql.createConnection({
 	port: 3306,
 	host: 'localhost',
 	user: 'root',
 	password: 'My1831my.',
 	database: 'burgers_db'
 });
+
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'My1831my.',
+		database: 'burgers_db'
+	});	
+};
 
 connection.connect(function (err) {
 	if (err) {
